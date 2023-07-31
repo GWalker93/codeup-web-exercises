fff169efb75239e747f5cf2eba604bd567636f7f
Gerald Walker <gerald.e.walker05@gmail.com>
2023-07-31 09:24:13 -0500

    updated weekend work

--- js/external.js
+++ js/external.js
@@ -5,6 +5,41 @@ alert("Welcome to my Website!")
 let userInput = prompt("What is your favorite color?");
 
 alert("Awesome, mine is also " + userInput + "!")
+//.................................
+
+alert("Nice choice on movies. Lets figure out how much the rentals costed.")
+
+let LittleMermaid = prompt ("How many nights did you rent Little Mermaid?")
+
+let LittleMermaidTotal = (LittleMermaid *3)
+
+alert("Your Little Mermaid rental cost $" + LittleMermaidTotal + ".")
+
+let BrotherBear = prompt("How many nights did you rent Brother Bear?" )
+
+let BrotherBearTotal = (BrotherBear *3)
+
+alert("Your Brother Bear rental cost $" + BrotherBearTotal + ".")
+
+let Hercules = prompt("How many nights did you rent Hercules?")
+
+let HerculesTotal = (Hercules *3)
+
+alert("Your Hercules rental was $" + HerculesTotal + ".")
+
+let totalPrice = (LittleMermaidTotal) + (BrotherBearTotal) + (HerculesTotal)
+
+alert("The total price of the rentals will be $" + totalPrice)
+
+console.log("LittleMermaid Days Rented " + LittleMermaid)
+console.log("BrotherBear Days Rented " + BrotherBear)
+console.log("Hercules Days Rented " + Hercules)
+
+console.log("LittleMermaid Total Price " + LittleMermaidTotal)
+console.log("BrotherBear Total Price " + BrotherBearTotal)
+console.log("Hercules Days Rented " + HerculesTotal)
+
+console.log("Total Rental Price " + totalPrice)
 
 // ..................................................
 
@@ -106,19 +141,4 @@ if
 
 else ( (howManyShoes === true && premiumMembership === true) && offerValid === false
 )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
