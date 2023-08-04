eec545be2fe2973385e3da606b52e9f157501f9e
Gerald Walker <gerald.e.walker05@gmail.com>
2023-08-03 20:59:05 -0500

    Got stuck on 3

---
+++ js/for_loops.js
@@ -0,0 +1,35 @@
+//#1-2
+
+console.log("Hey");
+//
+//  // function showMultiplicationTable(i)
+// function showMultiplicationTable(x) {
+//
+//     for (let i = 1; i <= 10; i++) {
+//         let answer = (x * i);
+//         console.log(x + " x " + i + " = " + answer);
+//     }
+// }
+//
+// showMultiplicationTable(7)
+//
+
+//3
+// Generate a random number between 0 and 6
+// let luckyNumber = Math.floor(Math.random() * 6);
+
+let x=(3)
+luckyNumber = Math.floor(Math.random() * 100);
+
+    function numberOne(i){
+    function evenandodd(luckyNumber) {
+        for (let i = 1; i <= 20; i++) {
+            if  (i % 2 !== true)
+            { return "good"}
+            else (i % 2 == false)
+            { return "Bad" }
+                console.log("it")
+
+        }
+    }}
+luckyNumber(20)
