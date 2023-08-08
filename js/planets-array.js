ce9499fb72c961baf3f6f21f960c94901a15032c
Gerald Walker <gerald.e.walker05@gmail.com>
2023-08-08 10:26:54 -0500

    start of the day

--- js/planets-array.js
+++ js/planets-array.js
@@ -20,43 +20,69 @@
      * the step that it describes
      */
 
+    console.log("");
+
     console.log('Adding "The Sun" to the beginning of the planets array.');
     console.log(planets);
+
+    console.log("")
 
     planets.unshift("The Sun")
     console.log("Changes:" + planets);
 
+    console.log("")
+
     console.log('Adding "Pluto" to the end of the planets array.');
     console.log(planets);
+
+    console.log("")
 
     planets.push("Pluto")
     console.log("Changes:" + planets);
 
+    console.log("")
+
     console.log('Removing "The Sun" from the beginning of the planets array.');
     console.log(planets);
+
+    console.log("")
 
     planets.shift("The Sun")
     console.log("Changes:" + planets);
 
+    console.log("")
+
     console.log('Removing "Pluto" from the end of the planets array.');
     console.log(planets);
 
+    console.log("")
+
     planets.pop("Pluto")
     console.log("Changes:" + planets);
+
+    console.log("")
 
     console.log('Finding and logging the index of "Earth" in the planets array.');
 
     let index = planets.indexOf("Earth")
     console.log(index)
 
+    console.log("")
+
     console.log("Reversing the order of the planets array.");
     console.log(planets);
+
+    console.log("")
 
     planets.reverse()
     console.log("Changes:" + planets);
 
+    console.log("")
+
     console.log("Sorting the planets array.");
     console.log(planets);
+
+    console.log("")
 
     planets.sort()
     console.log("Changes:" + planets);
--- js/planets-string.js
+++ js/planets-string.js
@@ -4,7 +4,9 @@
     console.log("Hi")
 
     var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
-    var planetsArray;
+
+
+    var planetsArray = planetsString.split(`|`);
 
     /**
      * TODO:
@@ -13,17 +15,39 @@
      * console.log planetsArray to check your work
      */
 
-    // console.log(planetsArray);
+    console.log(planetsString);
+    console.log(planetsArray);
 
     /**
      * TODO:
      * Create a string with <br> tags between each planet. console.log() your
      * results. Why might this be useful?
-     *
-     * BONUS:
+     */
+
+    var planetsString2 = planetsArray.join(`<br>`)
+    console.log(planetsString2);
+
+     /** BONUS:
      * Create another string that would display your planets in an undordered
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
+
+
+//  let newList =<ul>
+//      "<li>Mercury</li> <li>Venus</li> <li>Earth</li> <li>Mars</li> <li>Jupiter</li> <li>Saturn</li> <li>Uranus</li> <li>Neptune</li>" </ul>
+//
+// console.log(newList)
+
+    let planetListHTML = "<ul>";
+
+    for (let i = 0; i < planetsArray.lenght; i++) {
+        planetListHTML +=
+            `<li>${planetsArray[i]}</li>`;
+    }
+
+    planetListHTML += "</ul>"
+
+    console.log(planetListHTML)
 
 })();
