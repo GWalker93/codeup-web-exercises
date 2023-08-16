407bc98d97ab50d4be2b87ac2ad802c75c458c12
Gerald Walker <gerald.e.walker05@gmail.com>
2023-08-16 10:28:58 -0500

    Morning work done

--- js/warmup.js
+++ js/warmup.js
@@ -1,14 +1,39 @@
 "Hello Gang"
 
-function logUser(name) {
+// function logUser(name) {
+//     {
+//         if (typeof name !== "string")
+//             return false
+//     }
+//         return `${name} has logged in for the day.`
+//     }
+//
+// console.log(logUser("G"))
+// console.log(logUser(4))
+// console.log(logUser("Jake"))
+// console.log(logUser(true))
+
+ let userObject = {
+
+     userName : "Gerald",
+     email: "gerwal93@yahoo.com",
+     // userObject.roles ["admin", "moderator", "buyer"]
+ };
+
+// console.log(userObject.userName + " with email " + userObject.email + " has logged in for the day.")
+
+function logUser(object) {
     {
-        if (typeof name !== "string")
+        if (typeof object.userName !== "string" || typeof userObject !== "string")
             return false
     }
-        return `${name} has logged in for the day.`
+        return `${object.userName} with email ${object.email} has logged in for the day.`
     }
 
-console.log(logUser("G"))
-console.log(logUser(4))
-console.log(logUser("Jake"))
-console.log(logUser(true))
+    console.log(logUser(userObject))
+
+
+
+
+
+
