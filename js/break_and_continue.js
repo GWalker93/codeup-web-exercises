97ff7dd202386984b325eece4fd11b7dfbdd1ffe
Gerald Walker <gerald.e.walker05@gmail.com>
2023-08-31 21:43:24 -0500

    done for the night

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -5,47 +5,47 @@ console.log("")
 
 'use strict';
 
-// let userNumber = prompt( "Hey there, can you give me a odd number bettwen 1 and 50? ")
-// console.log("Number to skip is: " + userNumber )
-//
-// console.log("")
-//
-// for (var i = 1; i < 50; i++) {
-//
-//
-//     if (i == userNumber)
-//         console.log("Yikes! Skipping number: " + userNumber)
-//     if (i % 2 !== 1 || i == userNumber) {
-//         // number isn't even
-//         // odd numbers aren't as cool
-//         // skip the rest of the loop and continue with the next iteration
-//
-//
-//         // prompt("Please try again with an odd number.")
-//         continue;
-//     }
-//         console.log('Here is an odd number: ' + i);
-//
-//
-// }
-
-
-
 let userNumber = prompt( "Hey there, can you give me a odd number bettwen 1 and 50? ")
 console.log("Number to skip is: " + userNumber )
 
-for (var i = 1; i < 100; i++) {
+console.log("")
 
-    console.log('Loop counter is: ' + i);
+for (var i = 1; i < 50; i++) {
 
-    if (userNumber % 2 !==1 ) {
-        prompt("please try again")
-        // use the break keyword to exit from the while loop
-        break;
-        // nothing after the break will get processed
-        console.log('You will never see this line.');
+
+    if (i == userNumber)
+        console.log("Yikes! Skipping number: " + userNumber)
+    if (i % 2 !== 1 || i == userNumber) {
+        // number isn't even
+        // odd numbers aren't as cool
+        // skip the rest of the loop and continue with the next iteration
+
+
+        // prompt("Please try again with an odd number.")
+        continue;
     }
+        console.log('Here is an odd number: ' + i);
+
+
 }
 
+
+//
+// let userNumber = prompt( "Hey there, can you give me a odd number bettwen 1 and 50? ")
+// console.log("Number to skip is: " + userNumber )
+//
+// for (var i = 1; i < 20; i++) {
+//
+//     console.log('Loop counter is: ' + i);
+//
+//     if (userNumber % 2 !==1 ) {
+//         prompt("please try again")
+//         // use the break keyword to exit from the while loop
+//         break;
+//
+//         // nothing after the break will get processed
+//         console.log('You will never see this line.');
+//     }
+//  }
 
 
