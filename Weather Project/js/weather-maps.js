7851b2c88151253d991c8b6712a1e3f8e5efc1c2
Gerald Walker <gerald.e.walker05@gmail.com>
2023-09-13 16:59:12 -0500

    work for the day

--- Weather Project/js/weather-maps.js
+++ Weather Project/js/weather-maps.js
@@ -6,7 +6,7 @@ const map = new mapboxgl.Map({
     container: 'map', // container 
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
     center: [-158.07723575059174, 21.332496909270986], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    zoom: 12 // starting zoom
 });
 
 // Weather API Current
@@ -14,7 +14,7 @@ const BASE_CURRENT_WEATHER_URL = `https://api.openweathermap.org/data/3.0/onecal
 
 console.log(BASE_CURRENT_WEATHER_URL + `lat=${21.332496909270986}&lon=${-158.07723575059174}&appid=${WEATHER_MAP_KEY}&units=imperial`);
 
-$.get(BASE_CURRENT_WEATHER_URL + `lat=${21.332496909270986}&lon=${-158.07723575059174}&appid=${WEATHER_MAP_KEY}&units=imperial`).done((data) => console.log(data))
+$.get(BASE_CURRENT_WEATHER_URL + `lat=${21.332496909270986}&lon=${-158.07723575059174}&exclude=hourly,current&appid=${WEATHER_MAP_KEY}&units=imperial`).done((data) => console.log(data))
 //
 $.get(BASE_CURRENT_WEATHER_URL + `lat=${21.332496909270986}&lon=${-158.07723575059174}&appid=${WEATHER_MAP_KEY}&units=imperial`).done((data) => {
     // Day temp:
@@ -29,26 +29,36 @@ $.get(BASE_CURRENT_WEATHER_URL + `lat=${21.332496909270986}&lon=${-158.077235750
     console.log("wind speed: " + data.daily[0].wind_speed);
     // UV Index:
     console.log("UV Index: " + data.daily[0].uvi);
+    // Date
+    console.log("Date: " + data.daily[0].dt.toString);
 
 
 
-    let html = `
-    <div>
-    <div>Day/Night Temp: ${data.daily[0].temp.day.toFixed(0)}/${data.daily[0].temp.night.toFixed(0)}</div>
-    <div>Today's conditions: ${data.daily[0].summary}</div>
-    <div>Today's Humidity: ${data.daily[0].humidity}</div>
-    <div>Today's Wind Speed: ${data.daily[0].wind_speed}</div>
-    <div>Today's UV Index: ${data.daily[0].uvi}</div>
-    </div>`
+    let html = " "
+    // let stopingPoint = 4
 
-    // let html1 = `
-    // <div>
-    // <div>Current Temp: ${data.current.temp.toFixed(0)}</div>
-    // <div>Current Feels Like Temp: ${data.current.feels_like.toFixed(0)}</div>
-    // <div>Current Humidity: ${data.current.humidity}</div>
-    // <div>Current conditions: ${data.current.weather[0].description}</div>
-    // </div>`
 
-    $("#weather").html(html);
-    // $("#weather1").html(html);
+
+    for (let i =0; i < data.daily.length; i++)
+    {
+     html += ` 
+
+   
+        
+    <div style="border: 1px solid black">
+    <div>${data.daily[i].dt} </div>
+    <div>Day/Night Temp: ${data.daily[i].temp.day.toFixed(0)}/${data.daily[i].temp.night.toFixed(0)}</div>
+    <div>Today's conditions: ${data.daily[i].summary}</div>
+    <div>Today's Humidity: ${data.daily[i].humidity}</div>
+    <div>Today's Wind Speed: ${data.daily[i].wind_speed}</div>
+    <div>Today's UV Index: ${data.daily[i].uvi}</div>
+    </div> `
+
+        $("#weather").html(html);
+
+     if (i === 4) {
+         break;
+     }
+    }
+
 })
