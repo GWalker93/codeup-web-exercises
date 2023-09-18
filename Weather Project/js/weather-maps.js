dd7c6c0053ef87095ef236ec6b978de0977ab6d0
Gerald Walker <gerald.e.walker05@gmail.com>
2023-09-18 10:42:59 -0500

    project done

--- Weather Project/js/weather-maps.js
+++ Weather Project/js/weather-maps.js
@@ -13,9 +13,14 @@ const map = new mapboxgl.Map({
 
 const geocoder = new MapboxGeocoder({
     accessToken: mapbox_api_token,
-    mapboxgl: mapboxgl
+    // mapboxgl: mapboxgl
+
 });
-document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
+// document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
+
+// map.addControl( geocoder.on(`results`, function (result) {
+//     console.log(result)
+// }))
 
 const marker = new mapboxgl.Marker({ color: 'blue', rotation: 235,
     draggable: true
@@ -24,6 +29,18 @@ const marker = new mapboxgl.Marker({ color: 'blue', rotation: 235,
     .addTo(map);
 
 marker.on('dragend', onDragEnd);
+
+map.addControl(
+    geocoder.on(`result`, function (results) {
+        marker.setLngLat(results.result.center)
+        marker.addTo(map)
+        onDragEnd()
+    })
+)
+
+window.addEventListener("load", (event) => {
+    onDragEnd()
+});
 
 // Add geolocate control to the map.
 
@@ -39,15 +56,15 @@ map.addControl(
     })
 );
 
-
 // Weather API Current
 const BASE_CURRENT_WEATHER_URL = `https://api.openweathermap.org/data/3.0/onecall?`
-
 
 //Date Converter
 const epochConverter = (epoch)=>{
     return  new Date(epoch * 1000).toString().substring(4, 15)
 }
+
+//Location Conv
 
 function onDragEnd() {
     const lngLat = marker.getLngLat();
@@ -62,14 +79,11 @@ function onDragEnd() {
 
     let location = lat + " " + lng
 
-
     console.log(lat, lng)
-
 
     $.get(BASE_CURRENT_WEATHER_URL + `lat=` + lat + `&lon=` + lng + `&appid=${WEATHER_MAP_KEY}&units=imperial`).done((data) => {
         console.log(data);
     })
-
 
     $.get(BASE_CURRENT_WEATHER_URL + `lat=` + lat + `&lon=` + lng + `&appid=${WEATHER_MAP_KEY}&units=imperial`).done((data) => {
         // // Day temp:
@@ -90,19 +104,19 @@ function onDragEnd() {
 
         // let name1 = data.alerts[0].sender_name
 
-
         let html = " "
         // let stopingPoint = 4
 
         for (let i = 0; i < data.daily.length; i++) {
             html += `
 
-    <div style="border: 1px solid black">
+    <div style="border: 2px solid black">
     <div>${epochConverter(data.daily[i].dt)} </div>
     <hr>
     <div>Day/Night Temp: ${data.daily[i].temp.day.toFixed(0)}/${data.daily[i].temp.night.toFixed(0)}</div>
     <hr>
     <div>Today's conditions: ${data.daily[i].weather[0].description}</div>
+    <img src="${cloudCoverage(data.daily[i].weather[0].description)}">
     <hr>
     <div>Today's Humidity: ${data.daily[i].humidity}</div>
     <hr>
@@ -113,15 +127,80 @@ function onDragEnd() {
 
             $("#weather").html(html);
 
-
-            $("#h").html("Current Location: " + location);
-
             if (i === 4) {
                 break;
             }
         }
-
     })
+
+    function reverseGeocode(coordinates, token) {
+        var baseUrl = 'https://api.mapbox.com';
+        var endPoint = '/geocoding/v5/mapbox.places/';
+        return fetch(baseUrl + endPoint + coordinates.lng + "," + coordinates.lat + '.json' + "?" + 'access_token=' + token)
+            .then(function(res) {
+                return res.json();
+            })
+            // to get all the data from the request, comment out the following three lines...
+            .then(function(data) {
+                return data.features[0].place_name;
+            });
+    }
+    reverseGeocode({lat, lng}, mapbox_api_token).then(function(results) {
+        console.log(results)
+
+    $("#h").html("Current Location: " + results);
+})
+}
+
+// Icons
+
+let clearsky = `https://openweathermap.org/img/wn/01d.png`
+let fewclouds = `https://openweathermap.org/img/wn/02d.png`
+let scatteredclouds = `https://openweathermap.org/img/wn/03d.png`
+let brokenclouds = `https://openweathermap.org/img/wn/04d.png`
+let showerrain = `https://openweathermap.org/img/wn/09d.png`
+let rain = `https://openweathermap.org/img/wn/10d.png`
+let thunderstorm = `https://openweathermap.org/img/wn/11d.png`
+let snow = `https://openweathermap.org/img/wn/13d.png`
+let mist = `https://openweathermap.org/img/wn/50d.png`
+
+function cloudCoverage(x) {
+    if (x === `clear sky`)
+    {
+        return  clearsky
+    }
+    else if (x === `scattered clouds`)
+    {
+        return scatteredclouds
+    }
+    else if (x === `few clouds`)
+    {
+        return fewclouds
+    }
+    else if (x === `broken clouds`)
+    {
+        return brokenclouds
+    }
+    else if (x === `shower rain` || `light rain`)
+    {
+        return showerrain
+    }
+    else if (x === `rain`)
+    {
+        return rain
+    }
+    else if (x === `thunderstorm`)
+    {
+        return thunderstorm
+    }
+    else if (x === `snow`)
+    {
+        return snow
+    }
+    else if (x === `mist`)
+    {
+        return mist
+    }
 }
 
 
